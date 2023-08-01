3b7d7a3b01676aedb8b29cc619d37ba3d938021c
Alexander Slávik <slavik.alexander@seznam.cz>
2023-08-01 15:29:57 +0200

    cteni tramcu z LilyPondu

---
+++ scripts/get_beams.py
@@ -0,0 +1,62 @@
+import svgpathtools
+import cmath
+import subprocess
+from collections import deque
+from itertools import groupby
+import glob
+import tqdm
+import json
+import os
+
+def left_bound(p):
+    return min(pp.start.real for pp in p)
+
+lily_command = ["lilypond", "-fsvg", "-dbackend=cairo", "-dinclude-settings=settings.ly", "--output=tmp"]
+
+for fn in tqdm.tqdm(glob.glob("../lily_source/*.ly")):
+    subprocess.run(lily_command+[fn], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+    paths, attributes = svgpathtools.svg2paths("tmp.svg")
+
+    beams = [p for p, a in zip(paths, attributes) if "fill:rgb(0%,100%,0%)" in a["style"]]
+    if not beams:
+        continue
+
+    stems = [p for p, a in zip(paths, attributes) if "rgb(100%,0%,0%)" in a["style"]]
+    stems.sort(key=left_bound)
+
+    beam_pairs = [(p, left_bound(p)) for p in beams]
+    beam_pairs.sort(key=lambda x: x[1])
+    groups = [(l[0][0], len(l)) for l in (list(g) for _, g in groupby(beam_pairs, key=lambda x: x[1]))]
+
+    beams, sizes = zip(*groups)
+
+    beams = deque(beams)
+
+    starts = []
+    ends = []
+
+    curr_beam = beams.popleft()
+    in_beam = False
+
+    for i, stem in enumerate(stems):
+        meets = bool(len(curr_beam.intersect(stem)))
+        if (not meets) and in_beam:
+            ends.append(i-1)
+            in_beam = False
+            if beams:
+                curr_beam = beams.popleft()
+                meets = bool(len(curr_beam.intersect(stem)))
+            else:
+                break
+        if meets and (not in_beam):
+            starts.append(i)
+            in_beam = True
+
+    if in_beam:
+        ends.append(i)
+
+    with open("../beam_data/"+os.path.basename(fn)[:-3]+".json", "w", encoding="utf-8") as f:
+        json.dump(list(zip(starts, ends, sizes)), f)
+
+os.remove("tmp.svg")
