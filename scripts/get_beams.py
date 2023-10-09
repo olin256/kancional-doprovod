573ee2bcebc9eb96e5010f83953d8c035208c4fe
Alexander Slávik <slavik.alexander@seznam.cz>
2023-10-09 17:27:41 +0200

    prazdne listy pri nepritomnosti tramcu

--- scripts/get_beams.py
+++ scripts/get_beams.py
@@ -26,44 +26,47 @@ for fn in tqdm.tqdm(glob.glob("../lily_source/*.ly")):
 
     beams = [p for p, a in zip(paths, attributes) if "fill:rgb(0%,100%,0%)" in a["style"]]
     if not beams:
-        continue
+        outlist = []
 
-    stems = [p for p, a in zip(paths, attributes) if "rgb(100%,0%,0%)" in a["style"]]
-    stems.sort(key=left_bound)
+    else:
+        stems = [p for p, a in zip(paths, attributes) if "rgb(100%,0%,0%)" in a["style"]]
+        stems.sort(key=left_bound)
 
-    beam_pairs = [(p, left_bound(p)) for p in beams]
-    beam_pairs.sort(key=lambda x: x[1])
-    groups = [(l[0][0], len(l)) for l in (list(g) for _, g in groupby(beam_pairs, key=lambda x: x[1]))]
+        beam_pairs = [(p, left_bound(p)) for p in beams]
+        beam_pairs.sort(key=lambda x: x[1])
+        groups = [(l[0][0], len(l)) for l in (list(g) for _, g in groupby(beam_pairs, key=lambda x: x[1]))]
 
-    beams, sizes = zip(*groups)
+        beams, sizes = zip(*groups)
 
-    beams = deque(beams)
+        beams = deque(beams)
 
-    starts = []
-    ends = []
+        starts = []
+        ends = []
 
-    curr_beam = beams.popleft()
-    in_beam = False
+        curr_beam = beams.popleft()
+        in_beam = False
 
-    for i, stem in enumerate(stems):
-        meets = bool(len(curr_beam.intersect(stem)))
-        if (not meets) and in_beam:
-            ends.append(i-1)
-            in_beam = False
-            if beams:
-                curr_beam = beams.popleft()
-                meets = bool(len(curr_beam.intersect(stem)))
-            else:
-                break
-        if meets and (not in_beam):
-            starts.append(i)
-            in_beam = True
+        for i, stem in enumerate(stems):
+            meets = bool(len(curr_beam.intersect(stem)))
+            if (not meets) and in_beam:
+                ends.append(i-1)
+                in_beam = False
+                if beams:
+                    curr_beam = beams.popleft()
+                    meets = bool(len(curr_beam.intersect(stem)))
+                else:
+                    break
+            if meets and (not in_beam):
+                starts.append(i)
+                in_beam = True
 
-    if in_beam:
-        ends.append(i)
+        if in_beam:
+            ends.append(i)
+
+        outlist = list(zip(starts, ends, sizes))
 
     with open("../beam_data/"+os.path.basename(fn)[:-3]+".json", "w", encoding="utf-8") as f:
-        json.dump(list(zip(starts, ends, sizes)), f)
+        json.dump(outlist, f)
 
 os.remove("tmp.svg")
 os.remove("tmp.ly")
